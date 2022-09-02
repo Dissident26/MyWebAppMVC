a2a1d20f6ee3da021089095b7493e9f3bf31dacd
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-09-02 18:34:17 +0300

    add homework # 13

---
+++ WebApplication/WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            var book = new BookModelView("Война и мир", "Толстой Л.Н.", 666);
+
+            return View(book);
+        }
+    }
+}
---
+++ WebApplication/WebApplication1/Models/BookModelView.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class BookModelView
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public BookModelView(string name, string author, int numberOfPages)
+        {
+            Name = name;
+            Author = author;
+            NumberOfPages = numberOfPages;
+        }
+    }
+}
