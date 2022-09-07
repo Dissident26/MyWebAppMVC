1f6d3ae4c8fc6cda3e21e909ad63a020360020e7
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-09-07 18:32:28 +0300

    add homework # 14

---
+++ WebApplication2/WebApplication2/Controllers/AppController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet]
+        public IActionResult VetApplication()
+        {
+            ModelState.Clear();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VetApplication(DogViewModel model)
+        {
+            (string Name, int Age, string OwnerName, float Weight) = model;
+            Console.WriteLine($"Name: {Name}, Age: {Age}, OwnerName: {OwnerName}, Weight: {Weight}");
+            return VetApplication();
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/DogViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication2.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public string OwnerName { get; set; }
+        public int Age { get; set; }
+        public float Weight { get; set; }
+        public void Deconstruct(out string Name, out int Age, out string OwnerName, out float Weight)
+        {   // деструктуризация
+            Name = this.Name;
+            Age = this.Age;
+            OwnerName = this.OwnerName;
+            Weight = this.Weight;
+        }
+    }
+}
